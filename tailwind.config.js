fa5c3a740bdf568c767499d6bae2f8b8ddda9d91
aiboyyahya <khalifahadnan354@gmail.com>
2025-07-08 10:03:17 +0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./src/**/*.{html,js}"], // Ini penting
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slide: {
+					"0%": { transform: "translateX(0)" },
+					"100%": { transform: "translateX(-100%)" },
+				},
+       slidein: {
+					from: {
+						opacity: "0",
+						transform: "translateY(-10px)",
+					},
+					to: {
+						opacity: "1",
+						transform: "translateY(0)",
+					},
+				},
+      },
+      animation: {
+        slide: "slide 2s linear infinite",
+				slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
+      },
+    },
   },
   plugins: [],
 }
