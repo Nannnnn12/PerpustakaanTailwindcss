be131d022db6de91433c6356fb0b26d5985d8340
aiboyyahya <khalifahadnan354@gmail.com>
2025-07-10 22:40:40 +0700

    add dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,8 @@ module.exports = {
   content: ["./src/**/*.{html,js}"], // Ini penting
   theme: {
     extend: {
-      keyframes: {
-        slide: {
-					"0%": { transform: "translateX(0)" },
-					"100%": { transform: "translateX(-100%)" },
-				},
-       slidein: {
-					from: {
-						opacity: "0",
-						transform: "translateY(-10px)",
-					},
-					to: {
-						opacity: "1",
-						transform: "translateY(0)",
-					},
-				},
-      },
-      animation: {
-        slide: "slide 2s linear infinite",
-				slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
-      },
+
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
